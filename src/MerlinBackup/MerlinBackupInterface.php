6e3ae5f26d0a8664c6f9f8c1ec457de452611c70
UCSDMath <UCSDMath.Project@gmail.com>
2017-05-14 10:41:47 -0700

    Update

--- src/MerlinBackup/MerlinBackupInterface.php
+++ src/MerlinBackup/MerlinBackupInterface.php
@@ -103,4 +103,6 @@ interface MerlinBackupInterface
      * @api
      */
     public function reset(): MerlinBackupInterface;
+
+    //--------------------------------------------------------------------------
 }
