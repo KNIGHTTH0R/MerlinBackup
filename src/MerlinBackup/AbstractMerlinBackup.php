5e2a610809ba178b2e8d4d60700f3eec8f56dc1e
UCSDMath <ucsdmath.project@gmail.com>
2017-07-17 16:35:39 -0700

    Update

--- src/MerlinBackup/AbstractMerlinBackup.php
+++ src/MerlinBackup/AbstractMerlinBackup.php
@@ -75,7 +75,7 @@ abstract class AbstractMerlinBackup implements MerlinBackupInterface, ServiceFun
      *
      * @api
      */
-    public const VERSION = '1.21.0';
+    public const VERSION = '1.22.0';
 
     //--------------------------------------------------------------------------
 
--- src/MerlinBackup/MerlinBackup.php
+++ src/MerlinBackup/MerlinBackup.php
@@ -43,7 +43,7 @@ class MerlinBackup extends AbstractMerlinBackup implements MerlinBackupInterface
      *
      * @api
      */
-    public const VERSION = '1.21.0';
+    public const VERSION = '1.22.0';
 
     //--------------------------------------------------------------------------
 
